6f7e194c3c07d5d8f442492a9847eb8f6d8584ee
Corpella <alessandrocordella97@gmail.com>
2022-11-28 20:12:30 +0100

    chore: init

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 120,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  semi: false,
+  useTabs: false,
+  tabWidth: 4,
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
