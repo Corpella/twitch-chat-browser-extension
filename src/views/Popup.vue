634cc80a073c582a5be163055bd9bcec8efa6993
Corpella <alessandrocordella97@gmail.com>
2023-01-08 01:08:45 +0100

    feat: update template to include date

--- src/views/Popup.vue
+++ src/views/Popup.vue
@@ -3,6 +3,12 @@ import { useChatHistory } from "@/composables/useChatHistory"
 
 const { init, parseMessage, state, currentMessages, currentChannel, channels, setCurrentChannel } = useChatHistory()
 
+const getFormattedDate = (date: string) =>
+    new Date(date).toLocaleDateString(navigator.language, {
+        hour: "2-digit",
+        minute: "2-digit",
+    })
+
 init(currentChannel.value)
 </script>
 
@@ -25,14 +31,17 @@ init(currentChannel.value)
         <main class="w-full bg-[#342e45] text-lg">
             <p class="p-5 text-center text-white" v-if="state.isLoading || !currentMessages.length">Loading</p>
             <ul v-else class="flex w-full flex-col gap-2">
-                <li v-for="({ color, message, name }, index) in currentMessages" :key="`${name}-${index}`">
+                <li v-for="({ color, message, name, createdAt }, index) in currentMessages" :key="`${name}-${index}`">
                     <div class="inline-block min-h-[2.5rem] w-full py-2 px-2">
+                        <span class="max-w-8 text-white\ my-auto min-w-[2rem] pr-1 align-middle text-[#bf94ff]">
+                            {{ `[${getFormattedDate(createdAt)}]` }}
+                        </span>
                         <span
-                            class="my-auto mr-1 text-center align-middle font-bold"
+                            class="my-auto px-1 text-center align-middle font-bold"
                             :style="{ color: color || '#7878ff' }"
                             >{{ name }}</span
                         >
-                        <span class="my-auto mr-1 font-bold">{{ ":" }}</span>
+                        <span class="my-auto pr-1 font-bold">{{ ":" }}</span>
                         <span class="text-white" v-html="parseMessage(message, currentChannel)" />
                     </div>
                 </li>
